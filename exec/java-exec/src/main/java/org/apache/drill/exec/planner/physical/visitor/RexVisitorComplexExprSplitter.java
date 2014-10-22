eace8fa06a3b5e947e10e3d3266502ecfacc839f
Jason Altekruse <altekrusejason@gmail.com>
2014-10-22 13:28:45 -0700

    WIP - fixing issues after rebase, still need to finish expression re-writing.
    reverse changes related to options not being found.

--- exec/java-exec/src/main/java/org/apache/drill/exec/planner/physical/visitor/RexVisitorComplexExprSplitter.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/planner/physical/visitor/RexVisitorComplexExprSplitter.java
@@ -17,5 +17,103 @@
  ******************************************************************************/
 package org.apache.drill.exec.planner.physical.visitor;
 
-public class RexVisitorComplexExprSplitter {
+import org.apache.drill.exec.expr.fn.FunctionImplementationRegistry;
+import org.apache.drill.exec.planner.types.RelDataTypeDrillImpl;
+import org.apache.drill.exec.planner.types.RelDataTypeHolder;
+import org.eigenbase.reltype.RelDataTypeFactory;
+import org.eigenbase.rex.RexBuilder;
+import org.eigenbase.rex.RexCall;
+import org.eigenbase.rex.RexCorrelVariable;
+import org.eigenbase.rex.RexDynamicParam;
+import org.eigenbase.rex.RexFieldAccess;
+import org.eigenbase.rex.RexInputRef;
+import org.eigenbase.rex.RexLiteral;
+import org.eigenbase.rex.RexLocalRef;
+import org.eigenbase.rex.RexNode;
+import org.eigenbase.rex.RexOver;
+import org.eigenbase.rex.RexRangeRef;
+import org.eigenbase.rex.RexVisitorImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RexVisitorComplexExprSplitter extends RexVisitorImpl<RexNode> {
+
+  RelDataTypeFactory factory;
+  FunctionImplementationRegistry funcReg;
+  List<RexNode> complexExprs;
+  int lastUsedIndex;
+
+  public RexVisitorComplexExprSplitter(RelDataTypeFactory factory, FunctionImplementationRegistry funcReg) {
+    super(true);
+    this.factory = factory;
+    this.funcReg = funcReg;
+    this.complexExprs = new ArrayList();
+    this.lastUsedIndex = 0;
+  }
+
+  @Override
+  public RexNode visitInputRef(RexInputRef inputRef) {
+    return null;
+  }
+
+  @Override
+  public RexNode visitLocalRef(RexLocalRef localRef) {
+    return null;
+  }
+
+  @Override
+  public RexNode visitLiteral(RexLiteral literal) {
+    return null;
+  }
+
+  @Override
+  public RexNode visitOver(RexOver over) {
+    return null;
+  }
+
+  @Override
+  public RexNode visitCorrelVariable(RexCorrelVariable correlVariable) {
+    return null;
+  }
+
+  public RexNode visitCall(RexCall call) {
+
+    String functionName = call.getOperator().getName();
+    int nArgs = call.getOperands().size();
+
+    if (funcReg.isFunctionComplexOutput(functionName) ) {
+      RexBuilder builder = new RexBuilder(factory);
+      RexNode ret = builder.makeInputRef( new RelDataTypeDrillImpl(new RelDataTypeHolder(), factory), lastUsedIndex);
+      lastUsedIndex++;
+      List<RexNode> newOps = new ArrayList();
+      RexNode r;
+      for (RexNode operand : call.operands) {
+        r = operand.accept(this);
+        if (r != null ) {
+          newOps.add(r);
+        } else {
+          newOps.add(operand);
+        }
+      }
+      return call.clone(new RelDataTypeDrillImpl(new RelDataTypeHolder(),factory), newOps);
+    }
+    return null;
+  }
+
+  @Override
+  public RexNode visitDynamicParam(RexDynamicParam dynamicParam) {
+    return null;
+  }
+
+  @Override
+  public RexNode visitRangeRef(RexRangeRef rangeRef) {
+    return null;
+  }
+
+  @Override
+  public RexNode visitFieldAccess(RexFieldAccess fieldAccess) {
+    return null;
+  }
+
 }
--- exec/java-exec/src/main/java/org/apache/drill/exec/planner/sql/handlers/DefaultSqlHandler.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/planner/sql/handlers/DefaultSqlHandler.java
@@ -53,6 +53,7 @@ import org.apache.drill.exec.planner.physical.visitor.MemoryEstimationVisitor;
 import org.apache.drill.exec.planner.physical.visitor.ProducerConsumerPrelVisitor;
 import org.apache.drill.exec.planner.physical.visitor.RelUniqifier;
 import org.apache.drill.exec.planner.physical.visitor.SelectionVectorPrelVisitor;
+import org.apache.drill.exec.planner.physical.visitor.SplitUpComplexExpressions;
 import org.apache.drill.exec.planner.physical.visitor.StarColumnConverter;
 import org.apache.drill.exec.planner.sql.DrillSqlWorker;
 import org.apache.drill.exec.server.options.OptionManager;
@@ -200,6 +201,7 @@ public class DefaultSqlHandler extends AbstractSqlHandler {
     // TODO - create a rewrite rule for dividing a project that has multiple complex writer outputs
     // pass in the planner settings referenced above
     phyRelNode = ((Prel) phyRelNode).accept(new RewriteProjectToFlatten(planner.getTypeFactory(), context.getDrillOperatorTable()), null);
+    phyRelNode = ((Prel) phyRelNode).accept(new SplitUpComplexExpressions(planner.getTypeFactory(), context.getDrillOperatorTable()), null);
     // Definitely before this one
     /*
      * 2.)
