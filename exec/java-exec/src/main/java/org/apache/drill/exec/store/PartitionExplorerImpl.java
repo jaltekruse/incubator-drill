40ac79ec70c9361a17ea308a37fc027c54150acd
Jason Altekruse <altekrusejason@gmail.com>
2015-02-03 12:13:22 -0800

    DRILL-2173: Partition queries to drive dynamic pruning
    Adds new interface on storage plugin registry as well as individual storage plugins for exploring workspaces.
    Adds injection for partition explorer to expression materialization and interpreted evaluation.
    
    New interface on storage plugin registry as well as individual storage plugins for exploring workspaces.
    
    Tasks remaining:
    - code cleanup
    - review if calcite rule fork is necessary
    - should at least put in in the drill fork
    - get timezone and query start time available to query context, currently created while running physical plan
    - prevents folding calls to now()
    - finish the switch statement in DrillConstExecutor to allow folding all drill types into literals
    
    Conflicts:
    	exec/java-exec/src/main/java/org/apache/drill/exec/ops/QueryContext.java
    
    2173 update -Address Chris' review comments.
    
    Change the PartitionExplorer to return an Iterable<String> instead of String[]
    
    Add interface level description to PartitionExplorer and StoragePluginPartitionExplorer.
    
    New inner class in FileSystemPlugin to fulfill the new Iterable interface for partitions.
    
    Formatting/cleanup fixes
    
    Clean up error reporting code in MaxDir UDF. Remove method to get a string from a DrillBuf, as it was already defined in StringFunctionHelpers. Add new utility method to specifically convert a VarCharHolder to a string to remove some boilerplate.
    
    Fixed an errant copy paste in a comment and removed unused imports.
    
    Fix docs in FileSystemPlugin, belongs with the 2173 changes.
    
    Fix references in Javadoc to properly use @link instead of @see.
    
    2173 fixes, correctly return an empty list of sub-partitions if the path requested in the partition explorer interface is a file. Fix a few docs.
    
    More 2173, finishing Chris' comments
    
    2173 update - Add validation for PartitionExplorer injectable in UdfUtiltiers.
    
    Conflicts:
    	exec/java-exec/src/main/java/org/apache/drill/exec/ops/FragmentContext.java
    
    small change to fix refactored unit tests.
    
    cleanup for 2173
    
    Fixing type protection and updating error message when retrieving the schema requested through the PartitionExplorer interface. Should not be an issue as all Schemas in Drill currently inherit from AbstractSchema (the Drill one, not the calcite one, this should definitely be renamed) but if this pattern changes in the future we will get a reasonably informative message.
    
    More cleanup.

---
+++ exec/java-exec/src/main/java/org/apache/drill/exec/store/PartitionExplorerImpl.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.apache.drill.exec.store;
+
+import net.hydromatic.optiq.SchemaPlus;
+
+import java.util.Collection;
+
+public class PartitionExplorerImpl implements PartitionExplorer {
+
+  private final SchemaPlus rootSchema;
+
+  public PartitionExplorerImpl(SchemaPlus rootSchema) {
+    this.rootSchema = rootSchema;
+  }
+
+  @Override
+  public Iterable<String> getSubPartitions(String schema,
+                                           String table,
+                                           Collection<String> partitionColumns,
+                                           Collection<String> partitionValues
+                                           ) throws PartitionNotFoundException {
+
+
+    SchemaPlus subSchema = rootSchema.getSubSchema(schema);
+    AbstractSchema typedSchema;
+    if (subSchema instanceof AbstractSchema) {
+      typedSchema = subSchema.unwrap(AbstractSchema.class);
+    } else {
+      throw new PartitionNotFoundException(String.format(
+          "Unexpected schema type, was expecting a instance of %s",
+          AbstractSchema.class.getSimpleName()));
+    }
+    return typedSchema.getSubPartitions(table, partitionColumns, partitionValues);
+  }
+}
