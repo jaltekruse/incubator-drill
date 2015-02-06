e4db116308afc2983eb612b5e1cff7a35101a9c3
Jason Altekruse <altekrusejason@gmail.com>
2015-02-06 10:43:38 -0800

    Remove RecordBatch from DrillFunc interface

--- exec/java-exec/src/main/codegen/templates/ConvertToNullableHolder.java
+++ exec/java-exec/src/main/codegen/templates/ConvertToNullableHolder.java
@@ -38,7 +38,7 @@ public class ${className} implements DrillSimpleFunc {
   @Param ${minor.class}Holder input;
   @Output Nullable${minor.class}Holder output;
 
-  public void setup(RecordBatch incoming) { }
+  public void setup() { }
 
   public void eval() {
     output.isSet = 1;
