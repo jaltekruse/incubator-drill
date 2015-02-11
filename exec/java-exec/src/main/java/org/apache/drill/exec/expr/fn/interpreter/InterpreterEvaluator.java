31571db6aee96967d06d084f6d531826669ad2a1
Jason Altekruse <altekrusejason@gmail.com>
2015-02-10 16:27:31 -0800

    Fix memory leak.

--- exec/java-exec/src/main/java/org/apache/drill/exec/expr/fn/interpreter/InterpreterEvaluator.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/expr/fn/interpreter/InterpreterEvaluator.java
@@ -48,7 +48,9 @@ import org.reflections.Reflections;
 
 import javax.inject.Inject;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 public class InterpreterEvaluator {
@@ -64,6 +66,8 @@ public class InterpreterEvaluator {
   public static void evaluate(int recordCount, UdfUtilities udfUtilities, RecordBatch incoming, ValueVector outVV, LogicalExpression expr) {
 
     InterpreterInitVisitor initVisitor = new InterpreterInitVisitor(udfUtilities);
+    initVisitor.reset();
+    List<DrillBuf> injectedBuffers = initVisitor.getManagedBuffers();
     InterEvalVisitor evalVisitor = new InterEvalVisitor(incoming, udfUtilities);
 
     expr.accept(initVisitor, incoming);
@@ -74,16 +78,40 @@ public class InterpreterEvaluator {
     }
 
     outVV.getMutator().setValueCount(recordCount);
+
+    List<DrillBuf> evaluationBuffers = evalVisitor.getManagedBuffers();
+    for (DrillBuf buf : injectedBuffers) {
+      buf.release();
+    }
+    for (DrillBuf buf : evaluationBuffers) {
+      buf.release();
+    }
   }
 
   public static class InterpreterInitVisitor extends AbstractExprVisitor<LogicalExpression, RecordBatch, RuntimeException> {
 
     private UdfUtilities udfUtilities;
+    private List<DrillBuf> managedBuffers;
 
     protected InterpreterInitVisitor(UdfUtilities udfUtilities) {
       super();
       this.udfUtilities = udfUtilities;
+      reset();
     }
+
+    /**
+     * While not currently re-used, this visitor has some state that should not be shared between uses,
+     * if a single instance is used to visit several expression trees, be sure to call this method between
+     * them.
+     */
+    public void reset() {
+      managedBuffers = new ArrayList<>();
+    }
+
+    public List<DrillBuf> getManagedBuffers() {
+      return managedBuffers;
+    }
+
     @Override
     public LogicalExpression visitFunctionHolderExpression(FunctionHolderExpression holderExpr, RecordBatch incoming) {
       if (! (holderExpr.getHolder() instanceof DrillSimpleFuncHolder)) {
@@ -105,7 +133,9 @@ public class InterpreterEvaluator {
 //          if ( f.getAnnotation(Inject.class) != null ) {
             f.setAccessible(true);
             if (f.getType().equals(DrillBuf.class)) {
-              f.set(interpreter, udfUtilities.getManagedBuffer());
+              DrillBuf buf = udfUtilities.getManagedBuffer();
+              managedBuffers.add(buf);
+              f.set(interpreter, buf);
             } else if (f.getType().equals(QueryDateTimeInfo.class)) {
               f.set(interpreter, udfUtilities.getQueryDateTimeInfo());
             } else if (f.getType().equals(PartitionExplorer.class)) {
@@ -141,21 +171,38 @@ public class InterpreterEvaluator {
   public static class InterEvalVisitor extends AbstractExprVisitor<ValueHolder, Integer, RuntimeException> {
     private RecordBatch incoming;
     private UdfUtilities udfUtilities;
+    private List<DrillBuf> managedBuffers;
 
     protected InterEvalVisitor(RecordBatch incoming, UdfUtilities udfUtilities) {
       super();
       this.incoming = incoming;
       this.udfUtilities = udfUtilities;
+      reset();
     }
 
     public DrillBuf getManagedBufferIfAvailable() {
+      DrillBuf ret;
       if (incoming != null) {
-        return incoming.getContext().getManagedBuffer();
+        ret = incoming.getContext().getManagedBuffer();
       } else {
-        return udfUtilities.getManagedBuffer();
+        ret = udfUtilities.getManagedBuffer();
       }
+      managedBuffers.add(ret);
+      return ret;
     }
 
+    /**
+     * While not currently re-used, this visitor has some state that should not be shared between uses,
+     * if a single instance is used to visit several expression trees, be sure to call this method between
+     * them.
+     */
+    public void reset() {
+      managedBuffers = new ArrayList<>();
+    }
+
+    public List<DrillBuf> getManagedBuffers() {
+      return managedBuffers;
+    }
 
     @Override
     public ValueHolder visitFunctionHolderExpression(FunctionHolderExpression holderExpr, Integer inIndex) {
