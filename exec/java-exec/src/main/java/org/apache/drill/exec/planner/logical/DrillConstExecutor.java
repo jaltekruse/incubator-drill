bf75c40d3b3274915bc32cbfefd436ad598f4eba
Jason Altekruse <altekrusejason@gmail.com>
2015-02-12 15:44:45 -0800

    Support more types in constant folding

--- exec/java-exec/src/main/java/org/apache/drill/exec/planner/logical/DrillConstExecutor.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/planner/logical/DrillConstExecutor.java
@@ -17,6 +17,8 @@
  ******************************************************************************/
 package org.apache.drill.exec.planner.logical;
 
+import net.hydromatic.avatica.ByteString;
+import org.apache.drill.common.exceptions.DrillRuntimeException;
 import org.apache.drill.common.expression.ErrorCollectorImpl;
 import org.apache.drill.common.expression.LogicalExpression;
 import org.apache.drill.exec.expr.ExpressionTreeMaterializer;
@@ -33,6 +35,8 @@ import org.eigenbase.relopt.RelOptPlanner;
 import org.eigenbase.rex.RexBuilder;
 import org.eigenbase.rex.RexNode;
 import org.eigenbase.util.NlsString;
+import org.joda.time.DateTime;
+import org.joda.time.Period;
 
 import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
@@ -61,13 +65,12 @@ public class DrillConstExecutor implements RelOptPlanner.Executor {
       if (errors.getErrorCount() != 0) {
         logger.error("Failure while materializing expression [{}].  Errors: {}", newCall, errors);
       }
-      // TODO - remove this restriction by addressing the TODOs below
+
       final MaterializedField outputField = MaterializedField.create("outCol", materializedExpr.getMajorType());
       ValueVector vector = TypeHelper.getNewVector(outputField, allocator);
       vector.allocateNewSafe();
       InterpreterEvaluator.evaluateConstantExpr(vector, udfUtilities, materializedExpr);
 
-      // TODO - add a switch here to translate expression results to the appropriate literal type
       try {
         switch(materializedExpr.getMajorType().getMinorType()) {
           case INT:
@@ -88,6 +91,64 @@ public class DrillConstExecutor implements RelOptPlanner.Executor {
           case BIT:
             reducedValues.add(rexBuilder.makeLiteral(((BitVector) vector).getAccessor().get(0) == 1 ? true : false));
             break;
+          case DECIMAL9:
+          case DECIMAL18:
+          case DECIMAL28SPARSE:
+          case DECIMAL38SPARSE:
+            reducedValues.add(rexBuilder.makeExactLiteral((BigDecimal) vector.getAccessor().getObject(0)));
+            break;
+          case DATE:
+            reducedValues.add(rexBuilder.makeDateLiteral(((DateTime)vector.getAccessor().getObject(0)).toCalendar(null)));
+            break;
+          case TIME:
+            // TODO - review the given precision value, chose the maximum available on SQL server
+            // https://msdn.microsoft.com/en-us/library/bb677243.aspx
+            reducedValues.add(rexBuilder.makeTimeLiteral(((DateTime) vector.getAccessor().getObject(0)).toCalendar(null), 7));
+          case TIMESTAMP:
+            // TODO - review the given precision value, could not find a good recommendation, reusing value of 7 from time
+            reducedValues.add(rexBuilder.makeTimestampLiteral(((DateTime) vector.getAccessor().getObject(0)).toCalendar(null), 7));
+            break;
+          case VARBINARY:
+            reducedValues.add(rexBuilder.makeBinaryLiteral(new ByteString((byte[]) vector.getAccessor().getObject(0))));
+            break;
+
+          // TODO - not sure how to populate the SqlIntervalQualifier parameter of the rexBuilder.makeIntervalLiteral method
+          // will make these non-reducible at planning time for now
+          case INTERVAL:
+          case INTERVALYEAR:
+          case INTERVALDAY:
+            // fall through for now
+//            reducedValues.add(rexBuilder.makeIntervalLiteral(((Period) vector.getAccessor().getObject(0)));
+
+          // TODO - map and list are used in Drill but currently not expressible as literals, these can however be
+          // outputs of functions that take literals as inputs (such as a convert_fromJSON with a literal string
+          // as input), so we need to identify functions with these return types as non-foldable until we have a
+          // literal representation for them
+          case MAP:
+          case LIST:
+            // fall through for now
+
+          // currently unsupported types
+          case TIMESTAMPTZ:
+          case TIMETZ:
+          case LATE:
+          case TINYINT:
+          case SMALLINT:
+          case GENERIC_OBJECT:
+          case NULL:
+          case DECIMAL28DENSE:
+          case DECIMAL38DENSE:
+          case MONEY:
+          case FIXEDBINARY:
+          case FIXEDCHAR:
+          case FIXED16CHAR:
+          case VAR16CHAR:
+          case UINT1:
+          case UINT2:
+          case UINT4:
+          case UINT8:
+            throw new DrillRuntimeException("Unsupported type returned during planning time constant expression folding: "
+                + materializedExpr.getMajorType().getMinorType() );
         }
       } catch (UnsupportedEncodingException e) {
         throw new RuntimeException("Invalid string returned from constant expression evaluation");
