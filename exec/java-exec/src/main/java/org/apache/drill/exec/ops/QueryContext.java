057b1daf51551a3b7997cd0af1bf64f5ec41f423
Jason Altekruse <altekrusejason@gmail.com>
2015-02-27 09:30:11 -0800

    DRILL-2060: Enable constant folding during planning. HAD REBASE CONFLICTS IN QueryContext
    Moved out some UDF changes into the appropriate changeset.
    
    Fixes for constant folding.
    
    Did not close BufferManager in QueryContext
    
    Make QueryContext implement AutoClosable, remove unused imports
    
    Add query context close call to cleanup in foreman.

--- exec/java-exec/src/main/java/org/apache/drill/exec/ops/QueryContext.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/ops/QueryContext.java
@@ -17,13 +17,18 @@
  */
 package org.apache.drill.exec.ops;
 
+import java.io.IOException;
 import java.util.Collection;
 
+import io.netty.buffer.DrillBuf;
 import net.hydromatic.optiq.SchemaPlus;
 import net.hydromatic.optiq.jdbc.SimpleOptiqSchema;
 
 import org.apache.drill.common.config.DrillConfig;
+import org.apache.drill.common.exceptions.DrillRuntimeException;
 import org.apache.drill.exec.expr.fn.FunctionImplementationRegistry;
+import org.apache.drill.exec.memory.BufferAllocator;
+import org.apache.drill.exec.memory.OutOfMemoryException;
 import org.apache.drill.exec.planner.physical.PlannerSettings;
 import org.apache.drill.exec.planner.sql.DrillOperatorTable;
 import org.apache.drill.exec.proto.CoordinationProtos.DrillbitEndpoint;
@@ -33,25 +38,45 @@ import org.apache.drill.exec.server.options.OptionManager;
 import org.apache.drill.exec.server.options.QueryOptionManager;
 import org.apache.drill.exec.store.StoragePluginRegistry;
 
+// TODO - consider re-name to PlanningContext, as the query execution context actually appears
+// in fragment contexts
+
 // TODO except for a couple of tests, this is only created by Foreman
 // TODO the many methods that just return drillbitContext.getXxx() should be replaced with getDrillbitContext()
-public class QueryContext {
-  // private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(QueryContext.class);
+public class QueryContext implements AutoCloseable, UdfUtilities{
+  static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(QueryContext.class);
+
   private final DrillbitContext drillbitContext;
   private final UserSession session;
   private final OptionManager queryOptions;
   private final PlannerSettings plannerSettings;
   private final DrillOperatorTable table;
 
-  public QueryContext(final UserSession session, final DrillbitContext drllbitContext) {
-    this.session = session;
-    this.drillbitContext = drllbitContext;
+  // most of the memory consumed by planning is on-heap, as the calcite planning library
+  // represents plans as graphs of POJOs. An allocator is created for the QueryContext (
+  // which is used for planning time constant expression evaluation)
+  private final BufferAllocator allocator;
+  private final BufferManager bufferManager;
+  private static final int INITIAL_OFF_HEAP_ALLOCATION = 1024 * 1024;
+  private static final int MAX_OFF_HEAP_ALLOCATION = 16 * 1024 * 1024;
 
-    queryOptions = new QueryOptionManager(session.getOptions());
-    plannerSettings = new PlannerSettings(queryOptions, getFunctionRegistry());
+
+  public QueryContext(final UserSession session, final DrillbitContext drllbitContext) {
+    super();
+    this.drillbitContext = drllbitContext;
+    this.session = session;
+    this.queryOptions = new QueryOptionManager(session.getOptions());
+    this.plannerSettings = new PlannerSettings(queryOptions, getFunctionRegistry());
     plannerSettings.setNumEndPoints(drillbitContext.getBits().size());
-    table = new DrillOperatorTable(getFunctionRegistry());
+    this.table = new DrillOperatorTable(getFunctionRegistry());
+    try {
+      this.allocator = drllbitContext.getAllocator().getChildAllocator(null, INITIAL_OFF_HEAP_ALLOCATION, MAX_OFF_HEAP_ALLOCATION, false);
+    } catch (OutOfMemoryException e) {
+      throw new DrillRuntimeException("Error creating off-heap allocator for planning context.",e);
+    }
+    this.bufferManager = new BufferManager(this.allocator, null);
   }
+
 
   public PlannerSettings getPlannerSettings() {
     return plannerSettings;
@@ -59,6 +84,10 @@ public class QueryContext {
 
   public UserSession getSession() {
     return session;
+  }
+
+  public BufferAllocator getAllocator() {
+    return allocator;
   }
 
   public SchemaPlus getNewDefaultSchema() {
@@ -103,5 +132,27 @@ public class QueryContext {
 
   public DrillOperatorTable getDrillOperatorTable() {
     return table;
+  }
+
+  /**
+   * TODO - generate the query start time and record the current timezone
+   * at the start of planning instead of the start of physical plan materialization.
+   *
+   * @return
+   */
+  @Override
+  public QueryDateTimeInfo getQueryDateTimeInfo() {
+    throw new UnsupportedOperationException("Query start time not currently available during planning.");
+  }
+
+  @Override
+  public DrillBuf getManagedBuffer() {
+    return bufferManager.getManagedBuffer();
+  }
+
+  @Override
+  public void close() throws IOException {
+    bufferManager.close();
+    allocator.close();
   }
 }
