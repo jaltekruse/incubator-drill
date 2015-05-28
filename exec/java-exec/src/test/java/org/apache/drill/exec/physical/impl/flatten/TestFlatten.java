4f65fcd8fdd7f8e733bb66ead37632921fe76153
Jason Altekruse <altekrusejason@gmail.com>
2015-05-28 15:14:39 -0700

    example of using replace and concat to make varchar filled with comma spaparated data look like json

--- exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/flatten/TestFlatten.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/flatten/TestFlatten.java
@@ -44,6 +44,11 @@ public class TestFlatten extends BaseTestQuery {
   }
 
   @Test
+  public void flattenVarCharList() throws Exception {
+    test("select flatten(convert_fromJSON(concat(concat('[\"', `replace`(varchar_list, ',','\",\"')), '\"]'))) from cp.`/store/json/list_in_varchar.json`");
+  }
+
+  @Test
   public void drill1671() throws Exception{
     int rowCount = testSql("select * from (select count(*) as cnt from (select id, flatten(evnts1), flatten(evnts2), flatten(evnts3), flatten(evnts4), flatten(evnts5), flatten(evnts6), flatten(evnts7), flatten(evnts8), flatten(evnts9), flatten(evnts10), flatten(evnts11) from cp.`/flatten/many-arrays-50.json`)x )y where cnt = 2048");
     assertEquals(rowCount, 1);
