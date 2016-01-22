1e96fc9c88743c754be8466c15331385c4c79730
Jason Altekruse <altekrusejason@gmail.com>
2016-01-22 12:54:03 -0800

    Fix if statements for checkstyle compliance

--- exec/java-exec/src/main/java/org/apache/drill/exec/store/parquet/ParquetFormatConfig.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/store/parquet/ParquetFormatConfig.java
@@ -29,8 +29,12 @@ public class ParquetFormatConfig implements FormatPluginConfig{
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
 
     ParquetFormatConfig that = (ParquetFormatConfig) o;
 
